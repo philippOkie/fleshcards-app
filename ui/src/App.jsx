76829b94ca9fd80a8643d35968a2113510a4166a
filip <kriukphilipp@gmail.com>
2025-02-03 14:08:15 +0200

    added examples

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,17 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">Button</button>
+    <div className="flex justify-center">
+      <div className="card bg-neutral text-neutral-content w-96 p-8">
+        <div className="flex w-full flex-col border-opacity-50">
+          <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+            content
+          </div>
+          <div className="divider"></div>
+          <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+            content
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
