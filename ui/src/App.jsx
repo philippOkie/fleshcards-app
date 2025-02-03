ee125e83364f251cc9bf1f6c9d3049271d4bb9b4
filip <kriukphilipp@gmail.com>
2025-02-03 15:54:50 +0200

    added card to test

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,17 +1,9 @@
+import Card from "./components/Card";
+
 function App() {
   return (
-    <div className="flex justify-center">
-      <div className="card bg-neutral text-neutral-content w-96 p-8">
-        <div className="flex w-full flex-col border-opacity-50">
-          <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
-            content
-          </div>
-          <div className="divider"></div>
-          <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
-            content
-          </div>
-        </div>
-      </div>
+    <div className="flex justify-center ">
+      <Card props={{ forwardText: "hello", backText: "Привет" }}></Card>
     </div>
   );
 }
