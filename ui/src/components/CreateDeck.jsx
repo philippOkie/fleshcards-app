78fccd5ac31b4996533537b532a8112e6d2ae7a7
filip <kriukphilipp@gmail.com>
2025-02-06 23:54:23 +0200

    test

--- ui/src/components/CreateDeck.jsx
+++ ui/src/components/CreateDeck.jsx
@@ -6,32 +6,7 @@ function CreateDeck() {
     <div className="flex flex-col">
       <div className="flex flex-col pl-48 pr-48 pb-32 gap-4 overflow-y-auto flex-1">
         <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
-        <AddCardComp />
+
         <button className="btn btn-primary btn-block">ADD CARD</button>
       </div>
 
