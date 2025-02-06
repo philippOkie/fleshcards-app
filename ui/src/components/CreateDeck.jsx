c9852ca8a357cacd5908967e02e4cc45db0d5cf4
filip <kriukphilipp@gmail.com>
2025-02-06 23:25:40 +0200

    created a separate component

--- ui/src/components/CreateDeck.jsx
+++ ui/src/components/CreateDeck.jsx
@@ -1,4 +1,5 @@
 import AddCardComp from "./AddCardComp";
+import CreateDeckFooter from "./CreateDeckFooter";
 
 function CreateDeck() {
   return (
@@ -34,24 +35,7 @@ function CreateDeck() {
         <button className="btn btn-primary btn-block">ADD CARD</button>
       </div>
 
-      <footer className="flex flex-row justify-between bg-base-100 fixed text-center p-4  bottom-0 left-0 pl-12 pr-12 w-full">
-        <div className="flex flex-row gap-8">
-          <input
-            type="text"
-            placeholder="Type your deck name here!"
-            className="input w-full max-w-xs"
-          />
-
-          <input
-            type="text"
-            style={{ width: "500px" }}
-            placeholder='You can add topics here "@anatomy @english" etc.'
-            className="input w-full max-w-lg"
-          />
-        </div>
-
-        <button className="btn btn-lg btn-success">Save</button>
-      </footer>
+      <CreateDeckFooter />
     </div>
   );
 }
