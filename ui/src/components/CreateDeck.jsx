7d573a33be4a3e57d86bc8444581355011d6a43d
filip <kriukphilipp@gmail.com>
2025-02-06 19:42:03 +0200

    so header doesn't jump

--- ui/src/components/CreateDeck.jsx
+++ ui/src/components/CreateDeck.jsx
@@ -2,7 +2,7 @@ import AddCardComp from "./AddCardComp";
 
 function CreateDeck() {
   return (
-    <div className="flex flex-col pl-48 pr-48 pb-12 gap-4 h-screen overflow-y-auto">
+    <div className="flex flex-col pl-48 pr-48 pb-12 gap-4 overflow-y-auto">
       <AddCardComp />
       <button className="btn btn-primary btn-block">ADD CARD</button>
     </div>
