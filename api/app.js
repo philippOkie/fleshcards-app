79d8495c38c97041d54e233a33a13eb5718d4931
filip <kriukphilipp@gmail.com>
2025-02-07 12:20:29 +0200

    cors

--- api/app.js
+++ api/app.js
@@ -1,11 +1,19 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import userRouter from "./routes/user.js";
 import deckRouter from "./routes/deck.js";
 import cardRouter from "./routes/card.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 
