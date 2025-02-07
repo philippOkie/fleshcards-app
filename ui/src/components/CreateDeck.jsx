48db220e6594119198664f773e61b7578cc68634
filip <kriukphilipp@gmail.com>
2025-02-07 15:38:21 +0200

    display cards from the deck

--- ui/src/components/CreateDeck.jsx
+++ ui/src/components/CreateDeck.jsx
@@ -1,11 +1,25 @@
+import { useDeck } from "./DeckContext";
+
 import AddCardComp from "./AddCardComp";
 import CreateDeckFooter from "./CreateDeckFooter";
 
 function CreateDeck() {
+  const { unfinishedDeck } = useDeck();
+
+  if (!unfinishedDeck || !unfinishedDeck.cards) {
+    return <p>No unfinished deck found or no cards available.</p>;
+  }
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-col pl-48 pr-48 pb-32 gap-4 overflow-y-auto flex-1">
-        <AddCardComp />
+        {unfinishedDeck.cards.map((card, index) => (
+          <AddCardComp
+            key={index}
+            initialFrontText={card.textForward}
+            initialBackText={card.textBack}
+          />
+        ))}
 
         <button className="btn btn-primary btn-block">ADD CARD</button>
       </div>
