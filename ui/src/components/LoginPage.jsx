f729300b5a8499a3d5432a08924b84604cc98b51
filip <kriukphilipp@gmail.com>
2025-02-08 22:25:13 +0200

    tweaks

--- ui/src/components/LoginPage.jsx
+++ ui/src/components/LoginPage.jsx
@@ -3,29 +3,24 @@ function LoginPage() {
     <div className="card bg-neutral text-neutral-content w-250 p-8 pl-12 pr-12 gap-6">
       <div className="space-y-2">
         <div className="text-3xl font-bold">Login</div>
-        <div>Enter your email and password to log in!</div>
+        <div>Enter your Email / Login and Password to log in!</div>
       </div>
-      <div className="space-y-4">
+
+      <div className="space-y-2">
         <label className="form-control w-full">
           <div className="label">
             <span className="label-text">Email / Login</span>
           </div>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
+
+          <input type="text" className="input input-bordered w-full" />
         </label>
 
         <label className="form-control w-full">
           <div className="label">
             <span className="label-text">Password</span>
           </div>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
+
+          <input type="text" className="input input-bordered w-full" />
         </label>
 
         <button className="btn btn-block btn-accent !mt-6">LOGIN</button>
