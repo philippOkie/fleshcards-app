282ce339cb9ff534ada5c17a5d412f8b2cfc9d9c
filip <kriukphilipp@gmail.com>
2025-03-03 15:37:36 +0200

    update card learning ui

--- ui/src/components/Card.jsx
+++ ui/src/components/Card.jsx
@@ -1,21 +1,40 @@
-function Card({ showAnswerBtnClicked, props }) {
+function Card({
+  showAnswerBtnClicked,
+  forwardText,
+  backText,
+  imageUrl,
+  imgForward,
+  imgBack,
+}) {
   return (
-    <div className="card bg-neutral text-neutral-content w-290 p-4">
-      <div className="flex w-full flex-col border-opacity-50">
-        <div className="card bg-base-100 rounded-box grid h-80 place-items-center flex justify-center text-5xl">
-          {props?.forwardText}
+    <div className="card bg-transparent text-neutral-content w-290 p-4">
+      <div className="flex w-full flex-col border-opacity-50 ">
+        <div className="card bg-base-100 rounded-box grid h-80 flex flex-row items-center justify-center space-x-4 overflow-scroll gap-4">
+          {imgForward && (
+            <img
+              src={imgForward}
+              alt="Card Image"
+              className="rounded-box h-full object-cover w-64"
+            />
+          )}
+          <span className="text-3xl text-center w-auto">{forwardText}</span>
         </div>
 
         <div className="divider"></div>
 
         {showAnswerBtnClicked ? (
-          <div className="card bg-base-100 rounded-box grid h-80 place-items-center flex justify-center text-5xl">
-            {props?.backText}
+          <div className="card bg-base-100 rounded-box grid h-80 place-items-center flex justify-center overflow-scroll gap-4">
+            {imgBack && (
+              <img
+                src={imgBack}
+                alt="Card Image"
+                className="rounded-box h-full object-cover w-64"
+              />
+            )}
+            <span className="text-3xl text-center">{backText}</span>
           </div>
         ) : (
-          <div className="card rounded-box grid h-80 place-items-center flex justify-center text-5xl">
-            <span>Answer is here!</span>
-          </div>
+          <div className="card rounded-box grid h-80 place-items-center flex justify-center"></div>
         )}
       </div>
     </div>
