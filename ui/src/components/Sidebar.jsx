eb5ff0682e2154c0f7f29dca6e1bf0a993048e0f
filip <kriukphilipp@gmail.com>
2025-03-03 11:11:45 +0200

    adjust styles to new version, add fallback

--- ui/src/components/Sidebar.jsx
+++ ui/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
-function Sidebar({ decks }) {
+function Sidebar({ decks = [] }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedDeck, setSelectedDeck] = useState(null);
   const navigate = useNavigate();
@@ -29,32 +29,34 @@ function Sidebar({ decks }) {
     );
 
   return (
-    <div className="flex flex-col gap-6">
-      <div className="form-control pr-4">
+    <div className="flex flex-col gap-6 w-64">
+      <div className="form-control w-full">
         <input
           type="text"
           placeholder="Search by name or topics"
-          className="input input-bordered w-24 md:w-auto"
+          className="input input-bordered w-full"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
 
-      <div className="flex flex-col gap-2 h-162 overflow-y-auto border pr-4">
+      <div className="flex flex-col gap-2 h-96 overflow-y-auto">
         {filteredDecks.length > 0 ? (
           filteredDecks.map((deck) => (
             <button
               key={deck.id}
-              className={`btn ${
+              className={`btn w-full ${
                 deck.id === selectedDeck ? "btn-primary" : "btn-neutral"
               }`}
               onClick={() => handleDeckClick(deck.id)}
             >
-              {deck.name}
+              <span className="truncate block w-full text-start flex justify-center">
+                {deck.name}
+              </span>
             </button>
           ))
         ) : (
-          <p>No decks found</p>
+          <p className="text-center">No decks found</p>
         )}
       </div>
     </div>
