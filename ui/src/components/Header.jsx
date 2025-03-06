d3401fabef4cbfceeb08c26dab6ca03ee8c935a9
filip <kriukphilipp@gmail.com>
2025-03-06 11:37:36 +0200

    we will not add ability to view other users decks for now

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -86,8 +86,6 @@ function Header({ onLogout }) {
       <div className="navbar-start flex gap-2 items-center mb-2 mt-2 pl-10">
         <div className="text-4xl w-[120px]">Spacer</div>
 
-        <button className="btn btn-neutral w-32 !text-xl">Decks</button>
-
         <button
           className="btn btn-accent w-16 !text-2xl cursor-pointer"
           onClick={handleCreateOrNavigateDeck}
